bbd554b1b19df29dba762cadfd0b326bc960417d
Joshua Lau <dachshunduniverse@gmail.com>
2024-09-21 18:43:52 -0400

    Progress between game states

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<script>
+<script lang="ts">
     import ColorButton from "$lib/components/ColorButton.svelte";
 
     import Top from "$lib/components/Top.svelte";
@@ -7,6 +7,76 @@
     let round = 1;
     let subRound = 0;
     let state = "game";
+
+    const COLOR_THRESH_MAP: Record<string, number> = {
+        purple: 0.5,
+        green: 0.5,
+        blue: 0.5,
+        yellow: 0.5
+    };
+
+    const checkRound = () => {
+        switch (round) {
+            case 0:
+                state = "survey";
+                break;
+            case 1:
+                state = "game";
+                break;
+            case 2:
+                state = "survey";
+                break;
+            case 3:
+                state = "wheel";
+                break;
+            case 4:
+                state = "survey";
+                break;
+            case 5:
+                state = "game";
+                break;
+            case 6:
+                state = "survey";
+                break;
+            case 7:
+                state = "game";
+                break;
+            case 8:
+                state = "survey";
+                break;
+            default:
+                state = "done";
+        }
+    };
+
+    const recordSurvey = (score: number) => {
+        $surveyResults.push(score);
+        $surveyResults = $surveyResults;
+        round++;
+        checkRound();
+    };
+
+    const playSlotMachine = (color: string) => {
+        if (!COLOR_THRESH_MAP[color]) {
+            throw new Error(`Invalid color: ${color}`);
+        }
+
+        const threshold = COLOR_THRESH_MAP[color];
+        const result = Math.random();
+
+        if (result > threshold) {
+            console.log("You won!");
+        } else {
+            console.log("You lost!");
+        }
+
+        subRound++;
+        if (subRound === 10) {
+            round++;
+            subRound = 0;
+        }
+        checkRound();
+    };
 
     $: console.log($surveyResults);
     $: console.log($score);
@@ -17,18 +87,27 @@
     <main class="flex-1">
         {#if state === "game"}
             <div class="text-5xl text-center">
-                <h2>Select a button</h2>
+                <h2>Select a slot machine</h2>
             </div>
             <div class="h-1/2 w-full flex px-16 py-8 p-16 gap-8">
                 {#if round === 1}
-                    <ColorButton color="purple" emoji="🦄" />
-                    <ColorButton color="green" emoji="🦆" />
-                {:else if round === 2}
-                    <ColorButton color="blue" emoji="🦈" />
-                    <ColorButton color="orange" emoji="🦊" />
-                {:else if round === 3}
-                    <ColorButton color="red" emoji="🦀" />
-                    <ColorButton color="yellow" emoji="🐤" />
+                    <ColorButton
+                        on:click={() => playSlotMachine("purple")}
+                        color="purple"
+                        emoji="🦄" />
+                    <ColorButton
+                        on:click={() => playSlotMachine("green")}
+                        color="green"
+                        emoji="🦆" />
+                {:else if round === 5}
+                    <ColorButton
+                        on:click={() => playSlotMachine("blue")}
+                        color="blue"
+                        emoji="🦈" />
+                    <ColorButton
+                        on:click={() => playSlotMachine("yellow")}
+                        color="yellow"
+                        emoji="🐤" />
                 {:else}
                     Hi
                 {/if}
@@ -42,44 +121,57 @@
             <div class="h-1/3 w-full flex px-16 py-8 gap-4">
                 <button
                     on:click={() => {
-                        $surveyResults.push(1);
-                        $surveyResults = $surveyResults;
+                        recordSurvey(1);
                     }}
                     class="b neutral">
                     😞
                 </button>
                 <button
                     on:click={() => {
-                        $surveyResults.push(2);
-                        $surveyResults = $surveyResults;
+                        recordSurvey(2);
                     }}
                     class="b neutral">
                     🙁
                 </button>
                 <button
                     on:click={() => {
-                        $surveyResults.push(3);
-                        $surveyResults = $surveyResults;
+                        recordSurvey(3);
                     }}
                     class="b neutral">
                     😐
                 </button>
                 <button
                     on:click={() => {
-                        $surveyResults.push(4);
-                        $surveyResults = $surveyResults;
+                        recordSurvey(4);
                     }}
                     class="b neutral">
                     🙂
                 </button>
                 <button
                     on:click={() => {
-                        $surveyResults.push(5);
-                        $surveyResults = $surveyResults;
+                        recordSurvey(5);
                     }}
                     class="b neutral">
                     😀
                 </button>
+            </div>
+        {/if}
+
+        {#if state === "wheel"}
+            Wheel
+            <button
+                on:click={() => {
+                    round++;
+                    checkRound();
+                }}
+                class="b neutral">
+                Spin
+            </button>
+        {/if}
+
+        {#if state === "done"}
+            <div class="text-5xl text-center">
+                <h2>Thank you for playing!</h2>
             </div>
         {/if}
     </main>
