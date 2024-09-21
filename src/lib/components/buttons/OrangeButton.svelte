cd1118c7a4fbd1d5810f1aa4346db716192bfe27
Joshua Lau <dachshunduniverse@gmail.com>
2024-09-21 18:11:16 -0400

    Basic UI

---
+++ src/lib/components/buttons/OrangeButton.svelte
@@ -0,0 +1,1 @@
+<button class="orange b"> 🐅</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
 <script>
+    import BlueButton from "$lib/components/buttons/BlueButton.svelte";
+    import GreenButton from "$lib/components/buttons/GreenButton.svelte";
+    import OrangeButton from "$lib/components/buttons/OrangeButton.svelte";
+    import PurpleButton from "$lib/components/buttons/PurpleButton.svelte";
     import Top from "$lib/components/Top.svelte";
 
     let round = 0;
@@ -11,31 +15,11 @@
             <h2>Select a button</h2>
         </div>
         <div class="h-1/2 w-full flex gap-16 p-16">
-            <button class="green b"> 🦆</button>
-            <button class="blue b">🦈</button>
+            <BlueButton />
+            <OrangeButton />
+            <PurpleButton />
+            <GreenButton />
         </div>
     </main>
     <footer class="w-full h-16 bg-base-300"></footer>
 </div>
-
-<style lang="postcss">
-    .b {
-        @apply rounded-md flex-1 text-8xl duration-150;
-    }
-
-    .blue {
-        @apply bg-blue-500 hover:bg-blue-600;
-    }
-
-    .green {
-        @apply bg-green-500 hover:bg-green-600;
-    }
-
-    .red {
-        @apply bg-red-500 hover:bg-red-600;
-    }
-
-    .yellow {
-        @apply bg-yellow-500 hover:bg-yellow-600;
-    }
-</style>
