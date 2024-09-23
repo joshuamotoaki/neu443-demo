d0507637f45982d912ebbc7ab37fe35d561d45fb
Joshua Lau <dachshunduniverse@gmail.com>
2024-09-23 14:32:03 -0400

    Unbold top

--- src/lib/components/Top.svelte
+++ src/lib/components/Top.svelte
@@ -5,7 +5,7 @@
 <div class="navbar bg-base-300 w-full mb-12">
     <div class="w-full text-center flex items-center justify-center">
         {#if $isStarted}
-            <h2 class="font-bold text-xl">
+            <h2 class="text-xl">
                 Balance: ${$score}
             </h2>
         {/if}
