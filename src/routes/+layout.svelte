e52c339da117a308a6c9e3b15cbab096f2ebe13d
Joshua Lau <dachshunduniverse@gmail.com>
2024-09-23 14:31:44 -0400

    Add font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,6 @@
     import "../app.pcss";
 </script>
 
-<slot />
+<div class="font-lexend">
+    <slot />
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        "lexend": ["Lexend Variable", "sans-serif"],
+      }
+    },
   },
   plugins: [require('daisyui'),],
   daisyui: {
