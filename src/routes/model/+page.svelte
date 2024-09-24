3239d65b2ffe91f37e1234de457f1d73a1954551
Joshua Lau <dachshunduniverse@gmail.com>
2024-09-24 10:43:38 -0400

    Change title

--- src/routes/model/+page.svelte
+++ src/routes/model/+page.svelte
@@ -128,6 +128,10 @@
     }
 </script>
 
+<svelte:head>
+    <title>Interactive Model</title>
+</svelte:head>
+
 <div class="mx-auto w-11/12 max-w-5xl py-4">
     <h1 class="text-center text-3xl">Interactive Model</h1>
     <div class="w-full flex justify-center">
